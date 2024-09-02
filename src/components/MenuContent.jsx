b8768ec62768489e60f53c51681f5fef91cbe8c0
pablo <pablo.j98@gmail.com>
2024-09-02 16:18:00 -0400

    hide login btn

--- src/components/MenuContent.jsx
+++ src/components/MenuContent.jsx
@@ -13,7 +13,7 @@ function SideMenuContent({ isShowed, hideModal }) {
             <Link className='text-lg font-bold text-white md:hover:text-blue-500 duration-300' to={'/services'} onClick={() => handleClick()}>Sercivios</Link>
             <Link className='text-lg font-bold text-white md:hover:text-blue-500 duration-300' to={'/test'} onClick={() => handleClick()}>Contacto</Link>
             <Link className='text-lg font-bold text-white md:hover:text-blue-500 duration-300' to={'/test'} onClick={() => handleClick()}>Sobre Nosotros</Link>
-            <Link className='text-lg font-bold text-white' to={'/test'}>Login</Link>
+            {/* <Link className='text-lg font-bold text-white' to={'/test'}>Login</Link> */}
         </>
     )
 }
